e390968949cd0c0d5ab8c32cf29720b203bf1e65
kthompson5 <111643299+kthompson5@users.noreply.github.com>
2025-04-19 13:55:28 -0500

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
